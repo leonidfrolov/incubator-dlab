bd3aa28057422596a96d0593ccc99405622f9e7f
Leonid_Frolov1 <frolovleon@gmail.com>
2020-01-31 12:13:55 +0200

    DLAB-1449 added some prints, changed one lib. deleted unnecessary arguments, defined ensure_jre_jdk function, changed log file name, added condition if public ip address argument is defined, it will connect to that host

--- infrastructure-provisioning/src/keycloak/deploy_keycloak.py
+++ infrastructure-provisioning/src/keycloak/deploy_keycloak.py
@@ -26,13 +26,11 @@ from fabric.api import *
 import argparse
 import sys
 import os
-from dlab.common_lib import ensure_step
+from fabric.contrib.files import exists
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--os_user', type=str, default='')
-parser.add_argument('--private_ip_address', type=str, default='')
 parser.add_argument('--public_ip_address', type=str, default='')
-parser.add_argument('--hostname', type=str, default='')
 parser.add_argument('--keyfile', type=str, default='')
 parser.add_argument('--keycloak_realm_name', type=str, default='')
 parser.add_argument('--keycloak_user', type=str, default='')
@@ -43,9 +41,19 @@ keycloak_version = "8.0.1"
 templates_dir = './templates/'
 external_port = "80"
 internal_port = "8080"
+private_ip_address = "127.0.0.1"
+
+def ensure_jre_jdk(os_user):
+    if not exists('/home/' + os_user + '/.ensure_dir/jre_jdk_ensured'):
+        try:
+            sudo('apt-get install -y default-jre')
+            sudo('apt-get install -y default-jdk')
+            sudo('touch /home/' + os_user + '/.ensure_dir/jre_jdk_ensured')
+        except:
+            sys.exit(1)
 
 def configure_keycloak():
-    sudo('wget https://downloads.jboss.org/keycloak/' + keycloak_version + '/keycloak-' + keycloak_version + '.tar.gz')
+    sudo('wget https://downloads.jboss.org/keycloak/' + keycloak_version + '/keycloak-' + keycloak_version + '.tar.gz -O /tmp/keycloak-' + keycloak_version + '.tar.gz')
     sudo('tar -zxvf /tmp/keycloak-' + keycloak_version + '.tar.gz -C /opt/')
     sudo('ln -s /opt/keycloak-' + keycloak_version + ' /opt/keycloak')
     sudo('chown ' + args.os_user + ':' + args.os_user + ' -R /opt/keycloak-' + keycloak_version)
@@ -53,52 +61,61 @@ def configure_keycloak():
     put(templates_dir + 'realm.json', '/tmp/' + args.keycloak_realm_name + '-realm.json')
     sudo("sed -i 's|realm-name|" + args.keycloak_realm_name + "|' /tmp/" + args.keycloak_realm_name + "-realm.json")
     put(templates_dir + 'keycloak.conf', '/etc/keycloak/keycloak.conf')
-    sudo("sed -i 's|WILDFLY_BIND=|WILDFLY_BIND=" + args.private_ip_address + "|' /etc/keycloak/keycloak.conf")
+    sudo("sed -i 's|WILDFLY_BIND=|WILDFLY_BIND=" + private_ip_address + "|' /etc/keycloak/keycloak.conf")
     put(templates_dir + 'keycloak-server.service', '/etc/systemd/system/keycloak.service')
     sudo("sed -i 's|OS_USER|" + args.os_user + "|' /etc/systemd/system/keycloak.service")
     sudo("systemctl daemon-reload")
     sudo("systemctl enable keycloak-server")
-    sudo('bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/tmp/' + args.keycloak_realm_name + '-realm.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING -b ' + args.private_ip_address) #also starts standalone mode
+    sudo('bin/standalone.sh -Dkeycloak.migration.action=import -Dkeycloak.migration.provider=singleFile -Dkeycloak.migration.file=/tmp/' + args.keycloak_realm_name + '-realm.json -Dkeycloak.migration.strategy=OVERWRITE_EXISTING -b ' + private_ip_address) #also starts standalone mode
 
 def configure_nginx():
     sudo('apt install -y nginx')
     put(templates_dir + 'nginx.conf', '/etc/nginx/conf.d/nginx.conf')
     sudo("sed -i 's|external_port|" + external_port + "|' /etc/nginx/conf.d/nginx.conf")
     sudo("sed -i 's|internal_port|" + internal_port + "|' /etc/nginx/conf.d/nginx.conf")
-    sudo("sed -i 's|private_ip_address|" + args.private_ip_address + "|' /etc/nginx/conf.d/nginx.conf")
+    sudo("sed -i 's|private_ip_address|" + private_ip_address + "|' /etc/nginx/conf.d/nginx.conf")
     sudo("systemctl daemon-reload")
     sudo("systemctl enable nginx")
     sudo("systemctl start nginx")
 
 if __name__ == "__main__":
-    local_log_filename = "{}_{}_{}.log".format(os.environ['conf_resource'],
-                                               os.environ['project_name'],
-                                               os.environ['request_id'])
+    local_log_filename = "keycloak_deployment_script.log"
     local_log_filepath = "/logs/keycloak/" + local_log_filename
     logging.basicConfig(format='%(levelname)-8s [%(asctime)s]  %(message)s',
                         level=logging.DEBUG,
                         filename=local_log_filepath)
 
     print("Configure connections")
-    try:
-        env['connection_attempts'] = 100
-        env.key_filename = [args.keyfile]
-        env.host_string = '{}@{}'.format(args.os_user, args.public_ip_address)
-    except Exception as err:
-        print("Failed establish connection. Excpeption: " + str(err))
-        sys.exit(1)
+    if args.public_ip_address != '':
+        try:
+            env['connection_attempts'] = 100
+            env.key_filename = [args.keyfile]
+            env.host_string = '{}@{}'.format(args.os_user, private_ip_address)
+        except Exception as err:
+            print("Failed establish connection. Excpeption: " + str(err))
+            sys.exit(1)
+    else:
+        try:
+            env['connection_attempts'] = 100
+            env.key_filename = [args.keyfile]
+            env.host_string = '{}@{}'.format(args.os_user, args.public_ip_address)
+        except Exception as err:
+            print("Failed establish connection. Excpeption: " + str(err))
+            sys.exit(1)
 
     print("Install Java")
     ensure_jre_jdk(args.os_user)
 
     try:
         configure_keycloak()
+        print("installing Keycloak")
     except Exception as err:
         print("Failed keycloak install: " + str(err))
         sys.exit(1)
 
     try:
         configure_nginx()
+        print("installing nginx")
     except Exception as err:
         print("Failed nginx install: " + str(err))
         sys.exit(1)
